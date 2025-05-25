fca2067cf5c025f0af72b0886fe54a15c0c56782
Annie Xu <anniexu2001@gmail.com>
2025-05-25 16:53:51 -0500

    Property and PropertyImage entities, init repos

---
+++ realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package edu.kansal_wells_xu_pina.realestate_api.repositories;
+
+import edu.kansal_wells_xu_pina.realestate_api.entities.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<Property, Long> {
+}
