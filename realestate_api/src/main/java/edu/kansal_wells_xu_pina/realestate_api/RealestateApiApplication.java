792c83e340df5c28b06072d0a1a10bd42d21d519
Pragya Kansal <pragyakansal419@gmail.com>
2025-05-25 15:01:12 -0500

    Added initial files for project

---
+++ realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/RealestateApiApplication.java
@@ -0,0 +1,13 @@
+package edu.kansal_wells_xu_pina.realestate_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealestateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealestateApiApplication.class, args);
+	}
+
+}
---
+++ realestate_api/src/test/java/edu/kansal_wells_xu_pina/realestate_api/RealestateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.kansal_wells_xu_pina.realestate_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealestateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
