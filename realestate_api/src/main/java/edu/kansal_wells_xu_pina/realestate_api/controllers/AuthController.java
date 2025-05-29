6aeccfcfeb7f310aa99901422b2a724ea830516a
Pragya Kansal <pragyakansal419@gmail.com>
2025-05-29 13:35:04 -0500

    Added basic login/registration functionality

--- realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/controllers/AuthController.java
+++ realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/controllers/AuthController.java
@@ -1,11 +1,21 @@
 package edu.kansal_wells_xu_pina.realestate_api.controllers;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+import edu.kansal_wells_xu_pina.realestate_api.entities.User;
+import org.springframework.ui.Model;
+import edu.kansal_wells_xu_pina.realestate_api.services.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 
 @Controller
 @RequestMapping("/landing-page")
-public class UserController {
+public class AuthController {
+    private AuthService authService;
+
+    @Autowired
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
 
     @GetMapping("/")
     public String landingPage() {
@@ -20,5 +30,20 @@ public class UserController {
     @GetMapping("/register")
     public String displayRegistrationForm() {
         return "registration-form";
+    }
+
+    // Finish implementation
+    @PostMapping("/register")
+    public String userRegistration(@ModelAttribute User user, Model model) {
+        authService.registerUser(user);
+        return "redirect:/dashboard";
+    }
+
+
+    // Finish implementation
+    @PostMapping("/login")
+    public String loginUser(@ModelAttribute User user, Model model) {
+        authService.loginUser(user);
+        return "redirect:/dashboard";
     }
 }
---
+++ realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/services/AuthServiceImpl.java
@@ -0,0 +1,69 @@
+package edu.kansal_wells_xu_pina.realestate_api.services;
+import edu.kansal_wells_xu_pina.realestate_api.entities.User;
+import edu.kansal_wells_xu_pina.realestate_api.exceptions.InvalidUserParameterException;
+import edu.kansal_wells_xu_pina.realestate_api.repositories.UserRepository;
+import edu.kansal_wells_xu_pina.realestate_api.exceptions.AlreadyExistsException;
+
+@Service
+public class AuthServiceImpl {
+
+    private UserRepository userRepository;
+
+    @Autowired
+    public AuthServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public User registerUser(User newUser) {
+        User existingUser = userRepository.findByEmail(newUser.getEmail());
+        if (existingUser != null) {
+            throw new AlreadyExistsException("A user with the email: " + newUser.getEmail() + " already exists. Please login " +
+                    " to your existing account or create an account with a new email.");
+        }
+        validateUserRegistrationFields(newUser);
+        return newUser;
+    }
+
+    @Override
+    public User loginUser(User user) {
+        User existingUser = userRepository.findByEmail(user.getEmail());
+        if (existingUser == null) {
+            throw new InvalidUserParameterException("A user with the email: " + user.getEmail() + " could not be found in the database. " +
+                    " Please try again and login with an email that is already registered within our system.");
+        }
+
+        // Temporary password check until security part is implemented
+        // TO-DO (for Edith): compare encrypted passwords instead of plaintext passwords
+        if (!existingUser.getPassword().matches(user.getPassword())) {
+            throw new InvalidUserParameterException("The password entered is incorrect. Please try again with the correct password.");
+        }
+        return existingUser;
+    }
+
+    private void validateUserRegistrationFields(User newUser) {
+        if (newUser.getFirstName() == null || newUser.getFirstName().trim().isEmpty()) {
+            throw new InvalidUserParameterException("The user's first name should not be null or empty. Please try again with a valid input.");
+        }
+
+        if (newUser.getLastName() == null || newUser.getLastName().trim().isEmpty()) {
+            throw new InvalidUserParameterException("The user's last name should not be null or empty. Please try again with a valid input.");
+        }
+
+        if (newUser.getEmail() == null || newUser.getEmail().trim().isEmpty()) {
+            throw new InvalidUserParameterException("The user's email must contain an @ symbol, and it should not be null or empty. " +
+                    " Please try again with a valid input.");
+        }
+
+        // Correct format for email
+        String emailRegex = "^[\\w.-]+@[\\w.-]+\\.[a-zA-Z]{2,}$";
+        if (!newUser.getEmail().matches(emailRegex)) {
+            throw new InvalidUserParameterException("The email format is incorrect. Please try again with a valid input.");
+        }
+
+        if (newUser.getPassword() == null || newUser.getPassword().length() < 7 || newUser.getPassword().length() > 14 || !newUser.getPassword().matches(".*[^a-zA-Z0-9].*")) {
+            throw new InvalidUserParameterException("The user's password must contain at least 7 characters and at most 14 characters, " +
+                    " and it must have at least one special character. Please try again with a valid input.");
+        }
+    }
+}
