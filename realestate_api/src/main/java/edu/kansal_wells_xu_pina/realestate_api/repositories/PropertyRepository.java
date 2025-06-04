a53cac964e19123c25798c6defd3cf0db7ed27c8
Gregory Wells <gregrwells829@gmail.com>
2025-06-04 00:19:01 -0500

    added methods to AgentService, AgentServiceImpl, created UpdateProfileRequest in dtos package for editing Agent profile, added findByAgent() method to PropertyRepository

--- realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/repositories/PropertyRepository.java
+++ realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/repositories/PropertyRepository.java
@@ -1,7 +1,11 @@
 package edu.kansal_wells_xu_pina.realestate_api.repositories;
 
 import edu.kansal_wells_xu_pina.realestate_api.entities.Property;
+import edu.kansal_wells_xu_pina.realestate_api.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PropertyRepository extends JpaRepository<Property, Long> {
-}
+    List<Property> findByAgent(User agent);
+    }
