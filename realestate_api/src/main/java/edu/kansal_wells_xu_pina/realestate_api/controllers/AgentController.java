1422eb95f478292826f03553d99cf804e2ad6dbc
Gregory Wells <gregrwells829@gmail.com>
2025-06-05 20:58:41 -0500

    edited /managelistings endpoint in AgentController for testing of newly created managelistings.html template, Edited Manage Listing link in common-dashboard html template for testing

--- realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/controllers/AgentController.java
+++ realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/controllers/AgentController.java
@@ -1,17 +1,13 @@
 package edu.kansal_wells_xu_pina.realestate_api.controllers;
 
-import edu.kansal_wells_xu_pina.realestate_api.dtos.UpdateProfileRequest;
 import edu.kansal_wells_xu_pina.realestate_api.entities.Property;
-import edu.kansal_wells_xu_pina.realestate_api.entities.User;
 import edu.kansal_wells_xu_pina.realestate_api.services.AgentService;
-import org.aspectj.weaver.loadtime.Agent;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.security.access.AccessDeniedException;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/agent")
@@ -21,11 +17,13 @@ public class AgentController {
 
     public AgentController(AgentService agentService) {this.agentService = agentService;}
 
-    @PreAuthorize("hasRole('AGENT')")
+   /* @PreAuthorize("hasRole('AGENT')")
     @GetMapping({"/dashboard", "/"})
     public String agentDashboard() {
         return "common-dashboard";
     }
+
+    */
 
     /* Redundant profile endpoints - now using common profile management
     @PreAuthorize("hasRole('AGENT')")
@@ -48,22 +46,22 @@ public class AgentController {
     */
 
     @PreAuthorize("hasRole('AGENT')")
-    @GetMapping("/{id}/managelistings")
+    @GetMapping("/managelistings")
     public String manageListings(@PathVariable Long id, Model model) {
-        model.addAttribute("properties", agentService.getAgentProperties(id));
-        // return "managelistings";
-        return "temp";
+        List<Property> properties = agentService.getAgentProperties(id);
+        model.addAttribute("properties", properties);
+        return "agent/managelistings";
     }
 
     // TO-DO: template for agentaddproperty.html and addProperty method to handle the form submission
-    @PreAuthorize("hasRole('AGENT')")
+    //@PreAuthorize("hasRole('AGENT')")
     @GetMapping("/{id}/addproperty")
     public String addPropertyForm(@PathVariable Long id, Model model) {
         model.addAttribute("property", new Property());
         return "temp";
     }
 
-    @PreAuthorize("hasRole('AGENT')")
+   // @PreAuthorize("hasRole('AGENT')")
     @PostMapping("/{id}/addproperty")
     public String addNewProperty(@PathVariable Long id, @ModelAttribute Property newProperty, Model model) {
         agentService.addNewProperty(newProperty);
