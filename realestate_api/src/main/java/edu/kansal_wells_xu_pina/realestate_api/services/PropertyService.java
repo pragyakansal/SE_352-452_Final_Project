428966177fee2d34e4dbcfaa399838df4cf1f290
Annie Xu <anniexu2001@gmail.com>
2025-06-08 10:04:51 -0700

    Filter

--- realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/services/PropertyService.java
+++ realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/services/PropertyService.java
@@ -1,5 +1,6 @@
 package edu.kansal_wells_xu_pina.realestate_api.services;
 
+import edu.kansal_wells_xu_pina.realestate_api.dtos.PropertyFilterDto;
 import edu.kansal_wells_xu_pina.realestate_api.entities.Property;
 import edu.kansal_wells_xu_pina.realestate_api.entities.PropertyImage;
 
@@ -7,11 +8,11 @@ import java.io.IOException;
 import java.util.List;
 
 public interface PropertyService {
+    List<Property> getFilteredProperties(PropertyFilterDto filter);
+
     List<Property> findAll();
 
     Property findById(Long id);
 
     Property updateProperty(Property savedProperty);
-
-
 }
--- realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/services/PropertyServiceImpl.java
+++ realestate_api/src/main/java/edu/kansal_wells_xu_pina/realestate_api/services/PropertyServiceImpl.java
@@ -1,12 +1,16 @@
 package edu.kansal_wells_xu_pina.realestate_api.services;
 
+import edu.kansal_wells_xu_pina.realestate_api.dtos.PropertyFilterDto;
 import edu.kansal_wells_xu_pina.realestate_api.entities.Property;
 import edu.kansal_wells_xu_pina.realestate_api.exceptions.NotFoundException;
 import edu.kansal_wells_xu_pina.realestate_api.repositories.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class PropertyServiceImpl implements PropertyService{
@@ -37,5 +41,61 @@ public class PropertyServiceImpl implements PropertyService{
     public Property updateProperty(Property savedProperty) {
         propertyRepo.save(savedProperty);
         return savedProperty;
+    }
+
+    @Override
+    public List<Property> getFilteredProperties(PropertyFilterDto filter) {
+        List<Property> allProperties = propertyRepo.findAll();
+
+        List<Property> result = new ArrayList<>();
+        for (Property property : allProperties) {
+            boolean include = true;
+
+            // Min square footage check
+            if (filter.getMinSqFt() != null) {
+                if (property.getSize() < filter.getMinSqFt()) {
+                    include = false;
+                }
+            }
+
+            // Min price check
+            if (include && filter.getMinPrice() != null) {
+                if (property.getPrice() < filter.getMinPrice()) {
+                    include = false;
+                }
+            }
+
+            // Max price check
+            if (include && filter.getMaxPrice() != null) {
+                if (property.getPrice() > filter.getMaxPrice()) {
+                    include = false;
+                }
+            }
+
+            if (include) {
+                result.add(property);
+            }
+        }
+
+        String sortBy = filter.getSortBy();
+
+        if ("price_asc".equals(sortBy)) {
+            // Lowest price first
+            Collections.sort(result, (a, b) -> a.getPrice().compareTo(b.getPrice()));
+        }
+        else if ("price_desc".equals(sortBy)) {
+            // Highest price first
+            Collections.sort(result, (a, b) -> b.getPrice().compareTo(a.getPrice()));
+        }
+        else if ("sqft_asc".equals(sortBy)) {
+            // Smallest size first
+            Collections.sort(result, (a, b) -> a.getSize().compareTo(b.getSize()));
+        }
+        else if ("sqft_desc".equals(sortBy)) {
+            // Largest size first
+            Collections.sort(result, (a, b) -> b.getSize().compareTo(a.getSize()));
+        }
+
+        return result;
     }
 }
